89bd7ffdf17a71be83e8e02477f5807b2f5ebad5
tecnomascol <informacion@tecnomascol.com>
2025-10-31 10:10:25 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/cotizatuiphone/",
       server: {
         port: 3000,
         host: '0.0.0.0',
